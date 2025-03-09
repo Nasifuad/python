346e6de5b9bb3e2dcd01e439e8c796bcf8fa025b
Nasif Fuad <nasifuad007@gmail.com>
2025-03-09 22:57:01 +0600

    fffff

--- yoyo/hello.py
+++ yoyo/hello.py
@@ -1,3 +1,27 @@
-for animal in ["dog", "cat", "mouse"]:
-    # You can use format() to interpolate formatted strings
- print(f"I have a {animal}")
+class Node():
+    def _init_(self, data):
+        self.data= data
+        self.next= None
+        
+class LinkedList():
+    def _init_(self):
+        self.head= None
+    
+    def insert(self, data):
+        newNode= Node(data)
+        newNode.next= self.head
+        self.head= newNode
+        
+    def printList(self):
+        temp= self.head
+        while(temp):
+            print(temp.data)
+            temp= temp.next
+
+
+llist= LinkedList()
+llist.insert(1)
+llist.insert(2)
+llist.insert(3)
+
+llist.printList()
---
+++ yoyo/stack.py
@@ -0,0 +1,3 @@
+class Node():
+    def __call__(self, *args, **kwds):
+        return self 

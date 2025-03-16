7fe8de27c53fb3c1729723f6482515e282179632
Nasif Fuad <nasifuad007@gmail.com>
2025-03-16 16:29:07 +0600

    binary Search

---
+++ yoyo/bs.py
@@ -0,0 +1,19 @@
+arr= [1,23,44,55,44]
+
+# binary search
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+print(binary_search(arr, 434))
